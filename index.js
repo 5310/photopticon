f2afcc965da9b2a0e907534860fc68a477bfda85
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-11-25 16:22:00 +0530

    Started the index.

--- index.js
+++ index.js
@@ -1,77 +1,20 @@
-var fs = require('fs');
-var path = require('path');
+var markup = [];
+var albumNames = Object.getOwnPropertyNames( albums );
+for ( var i = 0; i < albumNames.length; i++ ) {
+	var albumName = albumNames[i];
+	markup.push( 
+		[ 'li', 
+		 [ 'div.album',
+		  [ 'a', 
+		   [ 'img', { src: albums[albumName][0] } ],
+		   albumName, 
+		   { href: 'photopticon.html?album='+albumName }
+		  ]
+		 ] 
+		] 
+	);
+}
+markup.unshift( 'ul' )
+markup = [ 'div', markup ];
 
-// Returns an object containing albums by directory name and an array of all photos within that directory by parsing the 'photos' folder.
-var parseAlbums = function() {
-
-	// Recursively parses the photo folder and returns a nested array of the photos in any directory within.
-	function getPhotosRecursively( path ) {
-		var albums = [];
-		fs.readdirSync( path ).forEach( function( filename ) {
-			var filePath = path+'/'+filename;
-			var file = fs.statSync( filePath );
-			if ( file.isFile() ) {
-				if ( /\.(jpg|jpeg|png|webp|gif)$/i.test( filename ) ) {
-					albums.push( filePath );
-				}
-			} else if ( file.isDirectory() ) {
-				albums.push( getPhotosRecursively( filePath ) );
-			}
-		} );
-		return albums;
-	}
-
-	// Receives the nested array of photos and flattens it into a set of albums by directory name and all photos within it.
-	var flattenRecursivePhotosToAlbums = function( recursivePhotos ) {
-		var albums = {};
-		var queue = recursivePhotos.slice(0);
-		while ( queue.length ) {
-			var element = queue.shift();
-			if ( typeof element === 'string' ) {
-				var dirname = path.relative( 'photos', path.dirname( element ) );
-				if ( !albums[dirname] ) {
-					albums[dirname] = [];
-				}
-				albums[dirname].push( element );
-			} else if ( element instanceof Array ) {
-				element.forEach( function( innerElement ) {
-					queue.push( innerElement );
-				} );
-			}
-		}
-		return albums;
-	};
-
-	// Actually does the thing.
-	return flattenRecursivePhotosToAlbums( getPhotosRecursively( 'photos' ) );
-	
-};
-
-// Parses the album.
-var albums = parseAlbums( 'photos' );
-
-//// Stores the parsed albums into a json file that may be read by the frontend.
-//fs.writeFile( 
-//	"albums.json", 
-//	JSON.stringify( albums ), 
-//	function( err ) {
-//		if( err ) { 
-//			console.log( err ); 
-//		} else {
-//			console.log( "Albums parsed succesfully and saved to `albums.json`." );
-//		}
-//	} 
-//);
-
-// Stores the parsed albums into a global object called 'albums' in a js file that will be linked by the frontend.
-fs.writeFile( 
-	"photos/albums.js", 
-	"var albums = " + JSON.stringify( albums ) + ";", 
-	function( err ) {
-		if( err ) { 
-			console.log( err ); 
-		} else {
-			console.log( "Albums parsed succesfully and saved to `photos/albums.js`." );
-		}
-	} 
-);
+shaven(	[ document.body, markup ] );
