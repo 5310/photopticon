cba5f701cecb787ff5d67b33c37b00065995816b
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-11-25 20:38:58 +0530

    Exposed parsing and storing functions.

--- photos-to-albums.js
+++ photos-to-albums.js
@@ -1,61 +1,56 @@
 var fs = require('fs');
 var path = require('path');
 
-// Does the actual thing, for reals.
-var photosToAlbums = function() {
+// Returns an object containing albums by directory name and an array of all photos within that directory by parsing the 'photos' folder.
+var parseAlbums = function( dirPath ) {
 
-	// Returns an object containing albums by directory name and an array of all photos within that directory by parsing the 'photos' folder.
-	var parseAlbums = function() {
-
-		// Recursively parses the photo folder and returns a nested array of the photos in any directory within.
-		function getPhotosRecursively( path ) {
-			var albums = [];
-			fs.readdirSync( path ).forEach( function( filename ) {
-				var filePath = path+'/'+filename;
-				var file = fs.statSync( filePath );
-				if ( file.isFile() ) {
-					if ( /\.(jpg|jpeg|png|webp|gif)$/i.test( filename ) ) {
-						albums.push( filePath );
-					}
-				} else if ( file.isDirectory() ) {
-					albums.push( getPhotosRecursively( filePath ) );
+	// Recursively parses the photo folder and returns a nested array of the photos in any directory within.
+	function getPhotosRecursively( dirPath ) {
+		var albums = [];
+		fs.readdirSync( dirPath ).forEach( function( filename ) {
+			var filePath = dirPath+'/'+filename;
+			var file = fs.statSync( filePath );
+			if ( file.isFile() ) {
+				if ( /\.(jpg|jpeg|png|webp|gif)$/i.test( filename ) ) {
+					albums.push( filePath );
 				}
-			} );
-			return albums;
-		}
-
-		// Receives the nested array of photos and flattens it into a set of albums by directory name and all photos within it.
-		var flattenRecursivePhotosToAlbums = function( recursivePhotos ) {
-			var albums = {};
-			var queue = recursivePhotos.slice(0);
-			while ( queue.length ) {
-				var element = queue.shift();
-				if ( typeof element === 'string' ) {
-					var dirname = path.relative( 'photos', path.dirname( element ) );
-					if ( !albums[dirname] ) {
-						albums[dirname] = [];
-					}
-					albums[dirname].push( element );
-				} else if ( element instanceof Array ) {
-					element.forEach( function( innerElement ) {
-						queue.push( innerElement );
-					} );
-				}
+			} else if ( file.isDirectory() ) {
+				albums.push( getPhotosRecursively( filePath ) );
 			}
-			return albums;
-		};
+		} );
+		return albums;
+	}
 
-		// Actually does the thing.
-		return flattenRecursivePhotosToAlbums( getPhotosRecursively( 'photos' ) );
-
+	// Receives the nested array of photos and flattens it into a set of albums by directory name and all photos within it.
+	var flattenRecursivePhotosToAlbums = function( recursivePhotos ) {
+		var albums = {};
+		var queue = recursivePhotos.slice(0);
+		while ( queue.length ) {
+			var element = queue.shift();
+			if ( typeof element === 'string' ) {
+				var dirname = path.relative( 'photos', path.dirname( element ) );
+				if ( !albums[dirname] ) {
+					albums[dirname] = [];
+				}
+				albums[dirname].push( element );
+			} else if ( element instanceof Array ) {
+				element.forEach( function( innerElement ) {
+					queue.push( innerElement );
+				} );
+			}
+		}
+		return albums;
 	};
 
-	// Parses the album.
-	var albums = parseAlbums( 'photos' );
+	// Actually does the thing.
+	return flattenRecursivePhotosToAlbums( getPhotosRecursively( dirPath ) );
 
-	// Stores the parsed albums into a global object called 'albums' in a js file that will be linked by the frontend.
+};
+
+// Stores the parsed albums into a global object called 'albums' in a js file that will be linked by the frontend.
+var storeAlbums = function( albums, filePath ) {
 	fs.writeFile( 
-		"photos/albums.js", 
+		filePath, 
 		"var albums = " + JSON.stringify( albums ) + ";", 
 		function( err ) {
 			if( err ) { 
@@ -65,11 +60,17 @@ var photosToAlbums = function() {
 			}
 		} 
 	);
-	
 };
 
-// Export the actual function.
+// Does the actual thing, for reals.
+var photosToAlbums = function() {
+	storeAlbums( parseAlbums( 'photos' ), "photos/albums.js" ) ;
+};
+
+// Exports.
 module.exports = photosToAlbums;
+module.exports.parseAlbums = parseAlbums;
+module.exports.storeAlbums = storeAlbums;
 
 // Invoke the actual thing if being ran directly.
 if ( !module.parent ) {
